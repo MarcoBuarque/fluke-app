8718f5f88dbb6587b238a0a331f34a2621cfb71b
MarcoBuarque <marco@getty.io>
2020-09-08 00:23:08 -0300

    - change app navigation

---
+++ src/scenes/DataDetail/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export const DataDetail = () => {
+  return (
+    <SafeAreaView>
+      <Text>DataDetail TESTTT</Text>
+    </SafeAreaView>
+  );
+};
+
+export default DataDetail;
---
+++ src/scenes/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>HOMEEEE TESTTT</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
