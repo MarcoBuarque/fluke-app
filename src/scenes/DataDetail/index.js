76bbabf520d49369f1d34f57c48a6426e37045e2
MarcoBuarque <marco@getty.io>
2020-09-08 00:31:03 -0300

    - add scenes navigation

--- src/scenes/DataDetail/index.js
+++ src/scenes/DataDetail/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
 
-export const DataDetail = () => {
+export const DataDetail = ({navigation}) => {
   return (
     <SafeAreaView>
       <Text>DataDetail TESTTT</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        style={{backgroundColor: 'red', padding: 16, marginTop: 10}}>
+        <Text>GO TO HOME</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
 
-export const Home = () => {
+export const Home = ({navigation}) => {
   return (
     <SafeAreaView>
       <Text>HOMEEEE TESTTT</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('DataDetail')}
+        style={{backgroundColor: 'red', padding: 16, marginTop: 10}}>
+        <Text> GO TO DATA DETAIL</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
