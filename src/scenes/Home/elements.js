6d54baca6da01141957e93a5e8d179e254d9bc38
MarcoBuarque <marco@getty.io>
2020-09-11 03:24:38 -0300

    - add react-native-elements - add react-native-vector-icons - update ItemLayout

--- src/scenes/Home/elements.js
+++ src/scenes/Home/elements.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {FlatList, TouchableOpacity} from 'react-native';
+import {FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import ProgressCircle from 'react-native-progress-circle';
+// import Icon from 'react-native-vector-icons/Feather';
+import {ListItem} from 'react-native-elements';
 
 // Design
 import * as Utils from './../../components/Utils';
@@ -11,46 +13,54 @@ import Colors from './../../utils/Style/Colors';
 
 // https://awesomeopensource.com/project/JesperLekland/react-native-svg-charts-examples
 
-export const ListItem = ({item}) => {
+export const Item = ({item}) => {
   const {title, usedData, totalData, dataType, onPress} = item;
   const percentageAvailable = ((totalData - usedData) / totalData) * 100;
   return (
-    <TouchableOpacity onPress={onPress} style={{paddingBottom: 10, flex: 1}}>
+    <TouchableOpacity onPress={onPress} style={styles.itemContent}>
       <Utils.Row
-        background="#000"
+        flex={1}
         padding={10}
         borderRadius={6}
-        flex={1}
         background={Colors.secondaryBackground}>
         <Utils.View flex={1}>
-          <Utils.View paddingBottom={6}>
-            <Utils.Text size={20} color={Colors.white}>
-              {title}
-            </Utils.Text>
-          </Utils.View>
-          <Utils.Row align="center">
-            <Utils.View paddingRight={20} flex={0.4}>
-              <Utils.Text size={12} numberOfLines={1} color={Colors.black}>
-                Disponíveis: {`${totalData - usedData} ${dataType}`}
-              </Utils.Text>
-              <Utils.Text size={12} numberOfLines={1} color={Colors.black}>
-                Consumidos: {`${usedData} ${dataType}`}
-              </Utils.Text>
-            </Utils.View>
-            <Utils.View flex={0.3}>
-              <ProgressCircle
-                percent={100 - percentageAvailable}
-                radius={20}
-                borderWidth={3}
-                color={Colors.red}
-                shadowColor="#6959CD"
-                bgColor={Colors.secondaryBackground}>
-                <Utils.Text size={8} color={Colors.black}>
-                  {percentageAvailable}%
+          <Utils.Row flex={1}>
+            <Utils.View flex={1}>
+              <Utils.View paddingBottom={6}>
+                <Utils.Text size={20} color={Colors.white}>
+                  {title}
                 </Utils.Text>
-              </ProgressCircle>
+              </Utils.View>
+              <Utils.Row align="center">
+                <Utils.View paddingRight={20} flex={0.5}>
+                  <Utils.Text size={12} numberOfLines={1} color={Colors.black}>
+                    Disponíveis: {`${totalData - usedData} ${dataType}`}
+                  </Utils.Text>
+                  <Utils.Text size={12} numberOfLines={1} color={Colors.black}>
+                    Consumidos: {`${usedData} ${dataType}`}
+                  </Utils.Text>
+                </Utils.View>
+                <Utils.View flex={0.3}>
+                  <ProgressCircle
+                    percent={100 - percentageAvailable}
+                    radius={20}
+                    borderWidth={3}
+                    color={Colors.red}
+                    shadowColor="#6959CD"
+                    bgColor={Colors.secondaryBackground}>
+                    <Utils.Text size={8} color={Colors.black}>
+                      {percentageAvailable}%
+                    </Utils.Text>
+                  </ProgressCircle>
+                </Utils.View>
+              </Utils.Row>
             </Utils.View>
           </Utils.Row>
+        </Utils.View>
+        <Utils.View style={styles.chevron}>
+          <Utils.View>
+            <ListItem.Chevron color={Colors.black} size={30} />
+          </Utils.View>
         </Utils.View>
       </Utils.Row>
     </TouchableOpacity>
@@ -74,7 +84,8 @@ export const List = ({list}) => {
   return (
     <FlatList
       data={list}
-      renderItem={ListItem}
+      // renderItem={(item) => renderItem(item)}
+      renderItem={Item}
       keyExtractor={(item, index) => `${item.title}-${index}`}
     />
   );
@@ -83,3 +94,12 @@ export const List = ({list}) => {
 List.prototype = {
   list: PropTypes.array.isRequired,
 };
+
+const styles = StyleSheet.create({
+  itemContent: {paddingBottom: 10, flex: 1},
+  chevron: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    fontSize: 50,
+  },
+});
